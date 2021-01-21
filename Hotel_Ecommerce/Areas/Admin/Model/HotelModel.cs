70cc156bb67f369b64e29bb14c9f4e0c3c56cb71
hacicoskun <hacicoskun07@gmail.com>
2021-01-21 20:56:12 +0300

    #2

---
+++ Hotel_Ecommerce/Areas/Admin/Model/HotelModel.cs
@@ -0,0 +1,15 @@
+﻿using Hotel_Ecoomerce.DAL.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Ecommerce.Areas.Admin.Model
+{
+    public class OtelEklemeveGuncelleme
+    {
+        public List<OdaOzellikTablosu> OdaOzellikleri { get; set; }
+        public List<OtelOzellikTablosu> OtelOzellikleri { get; set; }
+
+    }
+}
