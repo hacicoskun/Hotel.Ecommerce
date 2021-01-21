8d7c5a3692ec67a8ce88ea151f91290f2cd16fb4
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-01-21 21:20:15 +0300

    1.3
    1.3

---
+++ Hotel_Ecommerce.Helper/General/HelperMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Ecommerce.Helper.General
+{
+    public static class HelperMethods
+    {
+        public static string ToMD5Hash(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return null;
+
+            return Encoding.ASCII.GetBytes(str).ToMD5Hash();
+        }
+        public static string ToMD5Hash(this byte[] bytes)
+        {
+            if (bytes == null || bytes.Length == 0)
+                return null;
+
+            using (var md5 = MD5.Create())
+            {
+                return string.Join("", md5.ComputeHash(bytes).Select(x => x.ToString("X2")));
+            }
+        }
+    }
+}
--- Hotel_Ecommerce/Areas/Admin/Controllers/AdminLoginController.cs
+++ Hotel_Ecommerce/Areas/Admin/Controllers/AdminLoginController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Hotel_Ecommerce.Helper.General;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,7 +11,7 @@ using System.Web.Mvc;
 namespace Hotel_Ecommerce.Areas.Admin.Controllers
 {
     [RouteArea("Admin", AreaPrefix = "panel")]
-    public class AdminLoginController : Controller
+    public class AdminLoginController : AdminBaseController
     {
         [Route("login")]
         // GET: Admin/AdminLogin
@@ -19,11 +22,18 @@ namespace Hotel_Ecommerce.Areas.Admin.Controllers
         [Route("user-login")]
         public ActionResult LoginControl(string Username, string Password)
         {
+            string password = Password.ToMD5Hash();
+            if (_unitOfWork.LoginUsers.Any(x => x.KullaniciAdi == Username && x.Sifre == Password))
+            {
+                Session.Add("AdminUserID", _unitOfWork.LoginUsers.Find(x => x.KullaniciAdi == Username && x.Sifre == Password)._id);
+                Session.Add("AdminUserNameLastName", _unitOfWork.LoginUsers.Find(x => x.KullaniciAdi == Username && x.Sifre == Password));
+                return Json(HttpStatusCode.OK);
+            }
 
-
-            return View();
-
+            return Json(HttpStatusCode.InternalServerError);
 
         }
+
+        
     }
 }
