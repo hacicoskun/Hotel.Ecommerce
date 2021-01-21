95c269aa19861a38d0ada842a3432a3629c3c6ac
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-01-21 20:56:06 +0300

    login
    login

---
+++ Hotel_Ecommerce/Areas/Admin/Controllers/AdminLoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Hotel_Ecommerce.Areas.Admin.Controllers
+{
+    [RouteArea("Admin", AreaPrefix = "panel")]
+    public class AdminLoginController : BaseController
+    {
+        [Route("login")]
+        // GET: Admin/AdminLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("user-login")]
+        public ActionResult LoginControl(string Username, string Password)
+        {
+            
+           
+
+           
+
+        }
+    }
+}
