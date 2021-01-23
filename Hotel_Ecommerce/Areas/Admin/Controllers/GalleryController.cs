ba2fced48cd8ec8fe3bcb5f65baf5fe8e700bcc1
hacicoskun <hacicoskun07@gmail.com>
2021-01-23 14:17:53 +0300

    create otel #5

---
+++ Hotel_Ecommerce/Areas/Admin/Controllers/GalleryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace Hotel_Ecommerce.Areas.Admin.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Admin/Gallery
+        public ActionResult Gallery()
+        {
+            ViewBag.user_id = "123";
+            ViewBag.fileRoot = "\"/Uploads/\"";
+            ViewBag.serverRoot = "true";
+            ViewBag.serverMode = "false";
+            ViewBag.useFileTable = "false";
+            return View();
+        }
+
+        [Route("create-gallery-session")]
+        public ActionResult CreateGallerySession(string sessionID, string sliderFolder, string screenImage)
+        {
+            Session.Add("gallery_SliderPath_" + sessionID, sliderFolder);
+            Session.Add("gallery_ScreenImage_" + sessionID, sliderFolder + screenImage);
+
+            return Json(HttpStatusCode.OK);
+        }
+    }
+}
