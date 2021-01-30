0855d53636a8920b5a63d970a1d3bc408f605b3c
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-01-30 18:08:09 +0300

    turmenu get update 1
    turmenu get update 1

--- Hotel_Ecommerce/Areas/Admin/Controllers/MenuController.cs
+++ Hotel_Ecommerce/Areas/Admin/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
 ﻿using Hotel_Ecoomerce.DAL.Concrete;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,29 +17,80 @@ namespace Hotel_Ecommerce.Areas.Admin.Controllers
         {
             return View();
         }
-        [Route("create-turmenusu")]
+
+        [Route("create-tur-menusu-icerik")]
+        [ValidateInput(false)]
         public ActionResult CreateTurMenu(string Baslik,string İcerik)
         {
-            if (Baslik.Length>0 && İcerik.Length>0)
+            var deger = "0";
+        
+            
+            if (Baslik.Trim()!="" && İcerik.Trim()!="" && !_unitOfWork.TurMenusu.Any(x => x.Baslik == Baslik))
+            {            
+                    TurMenusu turmenusu = new TurMenusu
+                    {
+                        Baslik = Baslik,
+                        İcerik = İcerik,
+                        CreatedDate = DateTime.Now,
+                        IsDeleted = false,
+                        IsActive = true,
+
+                    };
+                    _unitOfWork.TurMenusu.Insert(turmenusu);
+                    _unitOfWork.Save();
+                    deger = "1";
+                    return Json(deger);
+                
+            }
+            else if (_unitOfWork.TurMenusu.Any(x=>x.Baslik==Baslik))
             {
-                TurMenusu turmenusu = new TurMenusu
-                {
-                    Baslik = Baslik,
-                    İcerik = İcerik,
-                    CreatedDate = DateTime.Now,
-                    IsDeleted = false,
-                    IsActive = true,
-                    UserID = _sessionUser._id
-                };
-                _unitOfWork.TurMenusu.Insert(turmenusu);
-                _unitOfWork.Save();
-                return Json(true);
+                UpdateTurMenu(Baslik, İcerik);
+                deger = "1";
+                return Json(deger);
+
             }
             else
             {
-                return Json(false);
+                return Json(deger);
             }
             
         }
+
+        [Route("get-tur-menusu-icerik")]
+        [ValidateInput(false)]
+        public ActionResult GetTurMenu(string Baslik)
+        {         
+            var Liste = _unitOfWork.TurMenusu.ToList();
+            var İcerik = Liste.Find(x => x.Baslik == Baslik);
+            var deger = İcerik.İcerik;
+            return Json(İcerik);
+            //return Content(JsonConvert.SerializeObject(deger), "application/json");
+
+        }
+        [Route("get-tur-menusu-icerik")]
+        [ValidateInput(false)]
+        public ActionResult UpdateTurMenu(string Baslik,string İcerik)
+        {
+            var deger = "0";
+
+            if (_unitOfWork.TurMenusu.Any(x=>x.Baslik.ToLower()==Baslik.ToLower()))
+            {
+                TurMenusu turmenusu = _unitOfWork.TurMenusu.Find(x => x.Baslik == Baslik);
+                turmenusu.CreatedDate = DateTime.Now;
+                turmenusu.Baslik = Baslik;
+                turmenusu.İcerik = İcerik;
+                _unitOfWork.TurMenusu.Update(turmenusu);
+                _unitOfWork.Save();
+                deger = "1";
+                return Json(deger);
+            }
+            else
+            {
+                return Json(deger);
+            }
+           
+
+        }
+
     }
 }
