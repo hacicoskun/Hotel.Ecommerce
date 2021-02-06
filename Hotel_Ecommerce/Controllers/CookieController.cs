3715346cf63b24c618451615dfe1ad9358596b5b
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-02-06 13:25:43 +0300

    footer
    footer

---
+++ Hotel_Ecommerce/Controllers/CookieController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Ecommerce.Controllers
+{
+    public class CookieController : Controller
+    {
+        // GET: Cookie
+        [Route("cookie")]
+        public ActionResult Cookie()
+        {
+            return View();
+        }
+    }
+}
