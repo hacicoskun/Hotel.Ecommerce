03d50e901572b71e1ed8a4b1e1104e8b96ad8544
hacicoskun <hacicoskun07@gmail.com>
2021-02-06 13:22:53 +0300

    otel filtreleme ve otel autocomplete

---
+++ Hotel_Ecommerce/Content/assest/autocompsearch/scripts/demo.js
@@ -0,0 +1,97 @@
+﻿
+
+var yurticioteller = [];
+var kibrisotelleri = [];
+
+
+var yurtdisiotellinkleri = [];
+var yurticiotellinkleri = [];
+var $mam = jQuery.noConflict();
+
+$mam.ajax({
+    url: '/aramasonuclari',
+    type: 'GET',
+    dataType: 'json',
+    success: function (response) {
+        for (var i = 0; i < response.length; i++) {
+            var item = response[i];
+            if (item.Location === "Yurt İçi Otelleri" || item.Location === "Türkiye") {
+                yurticioteller.push(item.Name + "   |   " + item.Adress);
+                yurticiotellinkleri.push('http://www.becomingtur.com/' + item.Link);
+            }
+            else {
+               
+                kibrisotelleri.push(item.Name + "-" + item.Adress);
+                yurtdisiotellinkleri.push('http://www.becomingtur.com/' + item.Link);
+            }
+        }
+        
+       
+
+
+        var sltryurtici = $mam.map(yurticioteller, function (team) { return { value: team, data: { category: 'Yurt içi Otelleri' } }; });
+        var salturydisi = $mam.map(kibrisotelleri, function (team) { return { value: team, data: { category: 'Kıbrıs Otelleri' } }; });
+        var teams = sltryurtici.concat(salturydisi);
+
+        // Initialize autocomplete with local lookup:
+        $mam('#autocomplete').devbridgeAutocomplete({
+            lookup: teams,
+            minChars: 2,
+            onSelect: function (suggestion) {
+
+                if (suggestion.data.category.indexOf("Yurt içi Otelleri") >= 0) {
+
+
+                    if ($mam.inArray(suggestion.value, yurticioteller) !== -1) {
+                        var index = yurticioteller.indexOf(suggestion.value);
+                        for (var i = 0; i < yurticiotellinkleri.length; i++) {
+                            if (i == index) {
+
+                                var deger = yurticiotellinkleri[i];
+                                var h = document.getElementById('hgidilenyer');
+                                h.value = deger;
+
+                            }
+                        }
+
+
+                    }
+                }
+                else if (suggestion.data.category.indexOf("Yurtdışı Otelleri") >= 0) {
+
+
+                    if ($mam.inArray(suggestion.value, kibrisotelleri) !== -1) {
+                        var index = kibrisotelleri.indexOf(suggestion.value);
+                        for (var i = 0; i < yurtdisiotellinkleri.length; i++) {
+                            if (i == index) {
+                                var deger = yurtdisiotellinkleri[i];
+                                var h = document.getElementById('hgidilenyer');
+                                h.value = deger;
+
+                            }
+                        }
+
+
+                    }
+                }
+
+            },
+
+
+            showNoSuggestionNotice: true,
+            noSuggestionNotice: 'Üzgünüz,Sonuç Bulunamadı.',
+            groupBy: 'category',
+            minChars: 2
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
---
+++ Hotel_Ecommerce/Controllers/Services/SearchAutoCompleteController.cs
@@ -0,0 +1,39 @@
+﻿using Hotel_Ecommerce.DAL.Concrete;
+using Hotel_Ecommerce.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Ecommerce.Controllers.Services
+{
+    public class SearchAutoCompleteController : BaseController
+    {
+        // GET: SearchAutoComplete
+        [Route("aramasonuclari")]
+        public ActionResult Index()
+        {
+            List<JsonData> Oteller = _unitOfWork.Oteller.Where(x => x.OtelAktifMi == true && !x.IsDeleted).Select(s => new JsonData
+            {
+                FilterType = 0,
+                Name = s.OtelAdi,
+                Link = "http://www.becomingtur.com/" + s.OtelLink,
+                Adress = s.Otelil + "," + s.Otelilce,
+                Location=s.OtelBolgesi
+            }).ToList();
+            return this.Json(Oteller, JsonRequestBehavior.AllowGet);
+
+        }
+        public class JsonData
+        {
+            public string Name { get; set; }
+            public string Link { get; set; }
+            public int FilterType { get; set; } 
+            public string Location { get; set; }
+
+            public string Adress { get; set; }
+        }
+    }
+}
