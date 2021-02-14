3f7cee2a75c01571b018791f4ace336f2fba6b62
hacicoskun <hacicoskun07@gmail.com>
2021-02-14 15:10:03 +0300

    otelfiltreleme

--- Hotel_Ecommerce/Content/assest/autocompsearch/scripts/demo.js
+++ Hotel_Ecommerce/Content/assest/autocompsearch/scripts/demo.js
@@ -1,13 +1,14 @@
 ﻿
 
 var yurticioteller = [];
-var kibrisotelleri = [];
+var yurtdisiotelleri = [];
 
 
 var yurtdisiotellinkleri = [];
 var yurticiotellinkleri = [];
 var $mam = jQuery.noConflict();
-
+var isOtel = false;
+var otelLink = "";
 $mam.ajax({
     url: '/aramasonuclari',
     type: 'GET',
@@ -15,22 +16,34 @@ $mam.ajax({
     success: function (response) {
         for (var i = 0; i < response.length; i++) {
             var item = response[i];
-            if (item.Location === "Yurt İçi Otelleri" || item.Location === "Türkiye") {
-                yurticioteller.push(item.Name + "   |   " + item.Adress);
-                yurticiotellinkleri.push('http://www.becomingtur.com/' + item.Link);
+            if (item.Location === "Türkiye Otelleri" || item.Location === "Türkiye") {
+                if (item.OtelMi === "1") {
+                    yurticioteller.push(item.Name + " | " + item.Adress);
+                    yurticiotellinkleri.push(item.Link);
+                }
+                else {
+                    yurticioteller.push(item.Adress);
+                    yurticiotellinkleri.push(item.Link);
+                }
             }
             else {
-               
-                kibrisotelleri.push(item.Name + "-" + item.Adress);
-                yurtdisiotellinkleri.push('http://www.becomingtur.com/' + item.Link);
+
+                if (item.OtelMi === "1") {
+                    yurtdisiotelleri.push(item.Name + " | " + item.Adress);
+                    yurtdisiotellinkleri.push(item.Link);
+                }
+                else {
+                    yurtdisiotelleri.push(item.Adress);
+                    yurtdisiotellinkleri.push(item.Link);
+                }
             }
         }
-        
-       
 
 
-        var sltryurtici = $mam.map(yurticioteller, function (team) { return { value: team, data: { category: 'Yurt içi Otelleri' } }; });
-        var salturydisi = $mam.map(kibrisotelleri, function (team) { return { value: team, data: { category: 'Kıbrıs Otelleri' } }; });
+
+
+        var sltryurtici = $mam.map(yurticioteller, function (team) { return { value: team, data: { category: 'Türkiye Otelleri' } }; });
+        var salturydisi = $mam.map(yurtdisiotelleri, function (team) { return { value: team, data: { category: 'Yurtdışı Otelleri' } }; });
         var teams = sltryurtici.concat(salturydisi);
 
         // Initialize autocomplete with local lookup:
@@ -38,19 +51,18 @@ $mam.ajax({
             lookup: teams,
             minChars: 2,
             onSelect: function (suggestion) {
-
-                if (suggestion.data.category.indexOf("Yurt içi Otelleri") >= 0) {
+                isOtel = false;
+                if (suggestion.value.split('|').length > 1) {
 
 
                     if ($mam.inArray(suggestion.value, yurticioteller) !== -1) {
                         var index = yurticioteller.indexOf(suggestion.value);
                         for (var i = 0; i < yurticiotellinkleri.length; i++) {
-                            if (i == index) {
+                            if (i === index) {
 
-                                var deger = yurticiotellinkleri[i];
-                                var h = document.getElementById('hgidilenyer');
-                                h.value = deger;
 
+                                isOtel = true;
+                                otelLink = yurticiotellinkleri[i];
                             }
                         }
 
@@ -60,10 +72,10 @@ $mam.ajax({
                 else if (suggestion.data.category.indexOf("Yurtdışı Otelleri") >= 0) {
 
 
-                    if ($mam.inArray(suggestion.value, kibrisotelleri) !== -1) {
-                        var index = kibrisotelleri.indexOf(suggestion.value);
+                    if ($mam.inArray(suggestion.value, yurtdisiotelleri) !== -1) {
+                        var index = yurtdisiotelleri.indexOf(suggestion.value);
                         for (var i = 0; i < yurtdisiotellinkleri.length; i++) {
-                            if (i == index) {
+                            if (i === index) {
                                 var deger = yurtdisiotellinkleri[i];
                                 var h = document.getElementById('hgidilenyer');
                                 h.value = deger;
@@ -87,8 +99,40 @@ $mam.ajax({
 });
 
 
+function OtelAra() {
+    var OtelveyaBolge = tjq("#autocomplete").val();
+    var GirisTarihi = tjq("#depart").val();
+    var CikisTarihi = tjq("#return").val();
+    var YetiskinSayisi = tjq("#dropYetiskinSayisi_MP_cBdLN29i2").val();
+    var CocukSayisi = tjq("#dropCocukSayisi_MP_cBdLN29i2").val();
+    var CocukYas1 = "0";
+    var CocukYas2 = "0";
+    if (CocukSayisi == "1") {
+        CocukYas1 = tjq("#dropCocukYas1 option:selected").val();
+    }
+    else if (CocukSayisi == "2") {
+        CocukYas1 = tjq("#dropCocukYas1 option:selected").val();
+        CocukYas2 = tjq("#dropCocukYas2 option:selected").val();
+    }
+    if (OtelveyaBolge.trim() != "" && GirisTarihi.trim() != "" && CikisTarihi.trim() != "" && CikisTarihi.trim() != "" && YetiskinSayisi.trim() != "") {
+        var type = OtelveyaBolge.split(',').length;
 
-
+        if (!isOtel) {
+            if (type == 2)/*Otel Bölge ve il arama*/ {
+                window.location.href = "/arama-sonuclari?OtelBolgesi=" + OtelveyaBolge.split(',')[1] + " Otelleri&Otelil=" + OtelveyaBolge.split(',')[0].replace(" Otelleri", "").trim();
+            }
+            else if (type == 3)/*Otel Bölge ve il arama*/ {
+                window.location.href = "/arama-sonuclari?OtelBolgesi=" + OtelveyaBolge.split(',')[2] + " Otelleri&Otelil=" + OtelveyaBolge.split(',')[0] + "&Otelilce=" + OtelveyaBolge.split(',')[1].replace(" Otelleri", "").trim();
+            }
+        }
+        else {
+            window.location.href = otelLink;
+        }
+    }
+    else {
+        alert("Lütfen otel,bölge ve giriş-çıkış tarihlerini kontrol ediniz.");
+    }
+}
 
 
 
---
+++ Hotel_Ecommerce/Models/Filtre.cs
@@ -0,0 +1,35 @@
+﻿using Hotel_Ecommerce.DAL.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Ecommerce.Models
+{
+    public class FiltreOtel
+    {
+        public FiltreCheckbox Checkboxs { get; set; }
+        public List<Oteller> Otels { get; set; }
+        public string Bolge { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+    }
+    public class FiltreCheckbox
+    {
+        public List<FiltreKonaklamaTipleri> KonaklamaTipleri { get; set; }
+        public List<FiltreOtelOzellikleri> OtelOzellikleri { get; set; }
+    }
+    // GET: Filter
+    public class FiltreKonaklamaTipleri
+    {
+        public string KonaklamaAdi { get; set; }
+        public string KonaklamaAdiValue { get; set; }
+    }
+    public class FiltreOtelOzellikleri
+    {
+        public string OlanakAdi { get; set; }
+        public int KacOteldeVar { get; set; }
+        public string OlanakveSayisi { get; set; }
+    }
+
+}
