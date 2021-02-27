0daf15161f715517075199b277f924d1672a1e41
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-02-27 12:43:04 +0300

    Tur içeriklerini yollama
    Tur içeriklerini yollama

---
+++ Hotel_Ecommerce/Controllers/GetTurMenuController.cs
@@ -0,0 +1,28 @@
+﻿using Hotel_Ecommerce.Areas.Admin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Ecommerce.Controllers
+{
+    public class GetTurMenuController : AdminBaseController
+    {
+        // GET: GetTurMenu
+        [Route("get-yurtici-tur-menu")]
+        public ActionResult YurtIciGetTurMenu()
+        {
+            var values = _unitOfWork.TurMenusu.Find(x => x.Baslik == "YurticiTurları");
+            ViewBag.valueIci = values.İcerik;
+            return View();
+        }
+        [Route("get-yurtdisi-tur-menu")]
+        public ActionResult YurtDisiGetTurMenu()
+        {
+            var values = _unitOfWork.TurMenusu.Find(x => x.Baslik == "YurtdisiTurları");
+            ViewBag.valueDisi = values.İcerik;
+            return View();
+        }
+    }
+}
